682b454077cf9e5fdc3b032413f1f75276c819c8
Vanshika <vanshhiiikaa@gmail.com>
2026-09-26 22:38:19 +0530

    Delete assignment query 4.sql

--- assignment query 4.sql
+++
@@ -1,9 +0,0 @@
-SELECT 
-    CustomerID AS Customer_ID,
-    CustomerName AS Customer_Name,
-    City AS City_Name,
-    Region AS Region_Name,
-    SignupDate AS Signup_Date
-FROM Customers
-WHERE CustomerName LIKE 'S%' 
-  AND City = 'Lucknow';
